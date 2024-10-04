84fb7eb2a592e33c4691fc6e154833055df18b27
lolsmybagelz <davidjin@ufl.edu>
2024-10-04 13:02:53 -0400

    Create lab5.cpp

---
+++ lab5.cpp
@@ -0,0 +1,19 @@
+//
+// Created by lolsm on 10/4/2024.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+#include "StringData.h"
+
+using namespace std;
+int linearsearch(vector<string> arr, int size, int target) {
+  cout <<  "Enter a string" << endl;
+}
+int main()
+ {
+    vector<string> s = getStringData();
+  cout << "Hello World!" << endl;
+  cout << "wea";
+ cout << "we";
+ }
