97d0d83e6c63f46bb159b3cf934cc0ff9c9b8d03
lolsmybagelz <davidjin@ufl.edu>
2024-10-05 18:43:58 -0400

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -5,15 +5,75 @@
 #include <string>
 #include <vector>
 #include "StringData.h"
-
 using namespace std;
-int linearsearch(vector<string> arr, int size, int target) {
-  cout <<  "Enter a string" << endl;
+
+
+int linearsearch(vector<string> v, string target) {
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarysearch(vector<string> v, string target) {
+    int low = 0;
+    int high = v.size() - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (v[mid] == target) {
+            return mid;
+        }
+        else if (v[mid] < target) {
+            low = mid + 1;
+        }else {
+            high = mid - 1;
+
+        }
+    }
+    return -1;
 }
 int main()
  {
     vector<string> s = getStringData();
-  cout << "Hello World!" << endl;
-  cout << "wea";
- cout << "we";
+
+    cout << "not_here:" << endl;
+    cout << "linear search:" << endl;
+    long long  x = systemTimeNanoseconds();
+    cout << linearsearch(s, "not_here");
+    cout << " Time: "<< systemTimeNanoseconds() - x << endl;
+
+    cout << "binary search:" << endl;
+    x = systemTimeNanoseconds();
+    cout << binarysearch(s, "not_here");
+    cout << " Time: " << systemTimeNanoseconds() - x << endl;
+
+    cout << endl;
+
+    cout << "mzzzz:" << endl;
+    cout << "linear search:" << endl;
+    x = systemTimeNanoseconds();
+    cout << linearsearch(s, "mzzzz");
+    cout << " Time: "<< systemTimeNanoseconds() - x << endl;
+
+    cout << "binary search:" << endl;
+    x = systemTimeNanoseconds();
+    cout << binarysearch(s, "mzzzz");
+    cout << " Time: " << systemTimeNanoseconds() - x << endl;
+
+    cout << endl;
+
+    cout << "aaaaa:" << endl;
+    cout << "linear search:" << endl;
+    x = systemTimeNanoseconds();
+    cout << linearsearch(s, "aaaaa");
+    cout << " Time: "<< systemTimeNanoseconds() - x << endl;
+
+    cout << "binary search:" << endl;
+    x = systemTimeNanoseconds();
+    cout << binarysearch(s, "aaaaa");
+    cout << " Time: " << systemTimeNanoseconds() - x << endl;
+
+    cout << endl;
  }
